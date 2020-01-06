032d575e4dec4738cf83e7fa05bd281d1b59426c
Zach Cross <zcross@squareup.com>
2020-01-06 10:44:12 -0500

    Backfila web UI: navbar color reflects environment

--- service/web/tabs/app/src/utilities/theme.ts
+++ service/web/tabs/app/src/utilities/theme.ts
@@ -11,13 +11,10 @@ const environmentColorLookup: IEnvironmentToColorLookup = {
   DEVELOPMENT: color.platinum,
   TESTING: color.purple,
   STAGING: color.yellow,
-  PRODUCTION: color.red
+  PRODUCTION: "#db2636"
 }
-
-export const backfilaRed = "#db2636"
 
 export const backfilaTheme: ITheme = {
   ...defaultTheme,
-  environmentToColor: environmentToColor(environmentColorLookup),
-  navbarBackground: backfilaRed
+  environmentToColor: environmentToColor(environmentColorLookup)
 }
